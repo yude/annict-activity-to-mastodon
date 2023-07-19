9a02784b955ac63709d04b01d366849845753add
yude <i@yude.jp>
2023-07-20 08:55:29 +0900

    feat: Support Mastodon

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"context"
 	"log"
 	"time"
 
 	"github.com/robfig/cron"
-	"github.com/sivchari/gotwtr"
 )
 
 var last_updated time.Time
 
 func main() {
 	// Init Annict related things
+	InitConfig()
+
 	data, err := fetch_annict()
 	if err != nil {
 		log.Fatal("Error: Something went wrong on startup. Exiting.")
@@ -20,10 +20,7 @@ func main() {
 
 	last_updated = time.Now().UTC()
 	log.Printf("✅ Annict に %s (ID: %d) としてログインしました。\n", data.Activities[0].User.Username, data.Activities[0].User.ID)
-	log.Printf("   %s (UTC) 以降のアクティビティを Twitter に投稿します。\n", last_updated.Format("2006/1/2 15:04:05"))
-
-	// Init Twitter API v2
-	twtr_client := GetTwitterClient()
+	log.Printf("   %s (UTC) 以降のアクティビティを Mastodon に投稿します。\n", last_updated.Format("2006/1/2 15:04:05"))
 
 	c := cron.New()
 	c.AddFunc("@every 5s", func() {
@@ -43,12 +40,10 @@ func main() {
 		formatted := format_data(target)
 
 		for i := 0; i < len(formatted); i++ {
-			log.Println("📝 ツイートします: " + formatted[i])
-			_, err := twtr_client.PostTweet(context.Background(), &gotwtr.PostTweetOption{
-				Text: formatted[i],
-			})
+			log.Println("📝 トゥートします: " + formatted[i])
+			err := PostToMastodon(formatted[i])
 			if err != nil {
-				log.Fatal(err)
+				log.Println("Error:", err)
 			}
 		}
 
---
+++ mastodon.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func PostToMastodon(toot string) error {
+	val := url.Values{}
+	val.Set("status", toot)
+
+	res, err := http.PostForm(conf.Credentials.MastodonCredentials.MastodonUrl+"/api/v1/statuses?access_token="+conf.Credentials.MastodonCredentials.MastodonToken, val)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			return errors.New("failed to retrieve error")
+		}
+		return nil
+	}
+
+	return nil
+}
